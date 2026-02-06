5e3db02614bf996769c2ba482e64be281029b0e0
mariami57 <mmitova3@gmail.com>
2026-02-06 17:35:27 +0200

    added more front end logic

--- static/js/frontend_quiz.js
+++ static/js/frontend_quiz.js
@@ -29,5 +29,50 @@ function renderQuestions(results = nul) {
         }
 
         qDiv.innerHTML = `<h3>$Pq.text</h3>`;
-    })
+
+        if (q.choices && q.choices.length > 0) {
+        const choicesDiv = document.createElement('div');
+            choicesDiv.className = 'choices';
+
+            q.choices.forEach(choice => {
+                const label = document.createElement('label');
+                label.innerHTML = `<input type="radio" name="q${q.id}" value="${choice.id}">${choice.text}`;
+                label.querySelector('input').addEventListener('change', () => {
+                    userAnswers[q.id] = {choice_id: choice_id};
+                });
+                choicesDiv.appendChild(label);
+            });
+
+            qDiv.appendChild(choicesDiv);
+        }
+
+        else {
+            const textarea = document.createElement('textarea');
+            textarea.rows =4;
+            textarea.style.width = '100%';
+            textarea.addEventListener('input', () => {
+                userAnswers[q.id] = {text_answer: textarea.value};
+            });
+            qDiv.appendChild(textarea);
+        }
+
+
+        if (results && results[q.id]) {
+            const r = results[q.id];
+
+            if (r.feedback) {
+            const feedback = document.createElement('div');
+            feedback.className = 'feedback';
+            feedback.textContent = `Score ${score}`;
+            qDiv.appendChild(feedback);
+
+            const ref = document.createElement('div');
+            ref.className = 'reference';
+            ref.textContent = r.feedback;
+            qDiv.appendChild(ref);
+            }
+        }
+
+        container.appendChild(qDiv);
+    });
 }
