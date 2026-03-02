e780ac0e50e9093f45a888639d08bbfd338a9cf1
mariami57 <mmitova3@gmail.com>
2026-03-02 17:13:49 +0200

    corrected settings for password update

--- LearningPythonTheorySystem/settings.py
+++ LearningPythonTheorySystem/settings.py
@@ -91,14 +91,16 @@ SPECTACULAR_SETTINGS = {
 
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_BACKEND = config('SECRET_KEY')
+
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ROOT_URLCONF = 'LearningPythonTheorySystem.urls'
 
